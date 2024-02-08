47e65da4e4dd4399afe9df2c291b0e50823781c3
jakob <jakob.jeraj7@gmail.com>
2024-02-08 09:46:05 +0000

    ok

--- circulos.js
+++ circulos.js
@@ -1,26 +1,45 @@
-main();
+const igralec = document.getElementById('igralec');
+const igra = document.getElementById('igra');
+const krogi = [];
 
-//
-// start here
-//
-function main() {
-    const canvas = document.querySelector("#glcanvas");
-    // Initialize the GL context
-    const gl = canvas.getContext("webgl");
+document.addEventListener("mousemove", premakni_igralca);
 
-    // Only continue if WebGL is available and working
-    if (gl === null) {
-        alert(
-            "Unable to initialize WebGL. Your browser or machine may not support it.",
-        );
-        return;
-    }
+function premakni_igralca(event) {
+    const x = event.clientX;
+    const y = event.clientY;
 
-    while (true) {
-
-        // Set clear color to black, fully opaque
-        gl.clearColor(0.0, 0.0, 0.0, 1.0);
-        // Clear the color buffer with specified clear color
-        gl.clear(gl.COLOR_BUFFER_BIT);
-    }
+    igralec.style.left = x + 'px';
+    igralec.style.top = y + 'px';
 }
+
+const st_krogov = 150;
+
+function main() {
+
+    for (let i = 0; i < st_krogov; i++) {
+        const krog = document.createElement('div');
+
+
+        const X = Math.random() * (window.innerWidth - 50) + 25;
+        const Y = Math.random() * (window.innerHeight - 50) + 25;
+
+        krog.style.left = X + 'px';
+        krog.style.top = Y + 'px';
+
+        krog.id = 'krog';
+        igra.appendChild(krog);
+        krogi.push(krog);
+
+    }
+    setInterval(() => {
+        for (let krog of krogi) {
+            const randomX = Math.random() * (window.innerWidth - 50) + 25;
+            const randomY = Math.random() * (window.innerHeight - 50) + 25;
+
+            krog.style.left = randomX + 'px';
+            krog.style.top = randomY + 'px';
+        }
+    }, 200);
+}
+
+window.addEventListener('load', main);
